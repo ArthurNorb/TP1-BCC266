ea54f0e053152ad14569c3aa46115e98b3b81bea
ArthurNorb <arthunorberto4@gmail.com>
2023-01-09 14:05:00 -0300

    iniciado projeto na main

--- TP1/main.c
+++ TP1/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include "cpu.h"
 
 int main()
 {
+    int tamanho_RAM;
+    Maquina maquina;
+    Instrucao *instrucoes;
+
+    printf("Iniciando máquina...");
+    liga(&maquina, instrucoes, tamanho_RAM);
+    desliga(&maquina);
+    printf("\nDesligando máquina...");
+
     return 0;
 }
